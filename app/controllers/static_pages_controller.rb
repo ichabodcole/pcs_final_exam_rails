209dea883c8bb44e63c86e211e0b6dda3eed0977
ichabodcole <ichabodcole@gmail.com>
2013-04-24 11:13:34 -0700

    Update guard to watch for changes to controllers, Point routes root to StaticPages about, StaticPages actions now render steve.erb in views/shared

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def about
+    @sender = "About"
+    render 'shared/steve'
   end
 
   def contact
+    @sender = "Contact"
+    render 'shared/steve'
   end
 end
